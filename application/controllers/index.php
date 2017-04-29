ce686b2a80c5ba6044aa99b349d5c6685306a71a
jaykpatel1996 <jaykpatel1996@gmail.com>
2017-04-29 15:48:14 +0530

    Created homepage
    Created homepage

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -1,7 +1,8 @@
+
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class index extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,8 +19,17 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+	 
+	 public function __construct()
+	 {
+	 parent::__construct();
+	 }
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('header');
+
+		
 	}
 }
+
+?>
